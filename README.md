80f7ddb0b5b869c59601f8896b304a2a124775bc
silvermannn <227780093+silvermannn@users.noreply.github.com>
2025-10-16 15:48:50 +0300

    Started classic board

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SmallQChess.hs
 Small quantum chess implementation in Haskell
+
+https://habr.com/ru/articles/402355/
+
+
--- app/Engine/Board.hs
+++ app/Engine/Board.hs
@@ -1,3 +1,20 @@
 module Engine.Board where
 
-data Board = Board
+import Data.Word
+import Data.Bit
+import qualified Data.Map as M
+
+data Figure = King | Qween | Rook | Knight | Bishop | Pawn deriving (Eq, Ord, Show)
+
+data Color = White | Black deriving (Eq, Ord, Show)
+
+-- Bitboard representation
+
+type Bitboard = Word64
+
+data ClassicBoard = ClassicBoard
+    {
+        boards :: M.Map (Figure, Color) Bitboard
+    } deriving (Eq, Show)
+
+
